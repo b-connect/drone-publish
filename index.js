2a149d17abeaf0eaa99c3f0c2500a8c8689357f0
Erik Seifert <erik.seifert@b-connect.de>
2017-06-06 17:49:58 +0200

    Update

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const Drone = require('drone-node');
-const plugin = new Drone.Plugin();
 
-// const REPO_NAME = process.env.DRONE_REPO_NAME;
-// const REPO_NAME = process.env.DRONE_REPO_NAME;
+const fs = require('fs-extra')
 
-console.log(process.env);
+console.log('Start publishing for hugo');
 
-// plugin.parse().then((params) => {
-//   console.log('params', params.repo);
-//
-//   // gets build and repository information for
-//   // the current running build
-//   const build = params.build;
-//   const repo  = params.repo;
-//
-//   // gets plugin-specific parameters defined in
-//   // the .drone.yml file
-//   const vargs = params.vargs;
-// });
+let repository = false;
+let hugo_archtype = 'project';
+let drone_build_event = process.env.DRONE_BUILD_EVENT;
+let drone_repo_owner = process.env.DRONE_REPO_OWNER;
+let drone_repo_name = process.env.DRONE_REPO_NAME;
+let drone_commit_ref = process.env.DRONE_COMMIT_REF;
+let drone_commit_sha = process.env.DRONE_COMMIT_SHA;
+let drone_commit_link = process.env.DRONE_COMMIT_LINK;
+let drone_commit_tag = process.env.DRONE_TAG;
+
+const workspace = process.env.DRONE_WORKSPACE;
+
+const execute = require('execute-shell-promise');
+
+if (process.env.PLUGIN_HUGO_ARCHTYPE) {
+    hugo_archtype = process.env.PLUGIN_HUGO_ARCHTYPE;
+}
+
+if (!process.env.DRONE_REPO_OWNER) {
+    drone_repo_owner = 'example';
+}
+
+if (!process.env.drone_repo_name) {
+    drone_repo_name = 'website';
+}
+
+console.log(__dirname);
+
+const project_docs_dir = [workspace, '.docs' , hugo_archtype , drone_repo_owner, drone_repo_name].join('/');
+const project_index = [project_docs_dir, 'index.md'].join('/');
+const project_commit_index =  [project_docs_dir, 'index.md'].join('/');
+
+
+
+let stats;
+try {
+    fs.statSync(project_index);
+} catch (ex) {
+    console.log('CREATE NEW ', project_index);
+    fs.ensureFileSync(project_index)
+    fs.writeFileSync(project_index,'+++\nrepository = "here"\n+++\n\n');
+}
+
+let content = fs.readFileSync(project_index).toString();
+content = content.split('+++\n\n');
+content[1] = '- new content\n' + content[1];
+fs.writeFileSync(project_index, content.join('+++\n\n'))
+console.log('End publishing for hugo');
