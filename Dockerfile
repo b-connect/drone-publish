7a172650851307b8b5b98968a95d09faa4088d5a
Erik Seifert <erik.seifert@b-connect.de>
2017-06-08 17:50:31 +0200

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM alpine:latest
 
-RUN apk --update add nodejs nodejs-npm
-RUN   rm -rf \/var/cache/apk/*
+RUN apk --update add nodejs nodejs-npm 
+RUN apk --update add gcc libgcc libstdc++ make expat expat-dev build-base g++ libc6-compat
+RUN apk --update add python 
+RUN node --version
 ADD ./index.js /root
+ADD ./lib /root/lib
 ADD ./package.json /root
 RUN npm -g install yarn
 WORKDIR /root
 RUN yarn install
+RUN apk del --purge gcc libgcc libstdc++ make expat expat-dev build-base g++ libc6-compat python
+RUN rm -rf \/var/cache/apk/*
+
 ENTRYPOINT ["node", "/root/index.js"]
--- index.js
+++ index.js
@@ -1,55 +1,100 @@
 
-const fs = require('fs-extra')
+const fs = require('fs-extra');
+const composer = require('./lib/composer');
+const hugo_archtype = 'project';
+
+require('dotenv').config();
+
+let env = {};
+
+for ( a in process.env ) {
+    if (process.env[a]) {
+        if (a.split('_')[0] == 'DRONE') {
+            env[a] = process.env[a];
+        }
+    }
+}
 
 console.log('Start publishing for hugo');
 
-let repository = false;
-let hugo_archtype = 'project';
-let drone_build_event = process.env.DRONE_BUILD_EVENT;
-let drone_repo_owner = process.env.DRONE_REPO_OWNER;
-let drone_repo_name = process.env.DRONE_REPO_NAME;
-let drone_commit_ref = process.env.DRONE_COMMIT_REF;
-let drone_commit_sha = process.env.DRONE_COMMIT_SHA;
-let drone_commit_link = process.env.DRONE_COMMIT_LINK;
-let drone_commit_tag = process.env.DRONE_TAG;
+workspace = env.DRONE_WORKSPACE;
 
-const workspace = process.env.DRONE_WORKSPACE;
-
-console.log(process.env);
-
-const execute = require('execute-shell-promise');
-
-if (process.env.PLUGIN_HUGO_ARCHTYPE) {
-    hugo_archtype = process.env.PLUGIN_HUGO_ARCHTYPE;
-}
-
-if (!process.env.DRONE_REPO_OWNER) {
-    drone_repo_owner = 'example';
-}
-
-if (!process.env.drone_repo_name) {
-    drone_repo_name = 'website';
-}
-
-console.log(__dirname);
-
-const project_docs_dir = [workspace, '.docs' , hugo_archtype , drone_repo_owner, drone_repo_name].join('/');
+const project_docs_dir = [workspace, '.docs' , hugo_archtype , env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/');
+const build_docs_dir = [workspace, '.docs' , 'post' , env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/');
+const build_doc_name = [build_docs_dir, env.DRONE_BUILD_NUMBER + '.md'].join('/');
 const project_index = [project_docs_dir, 'index.md'].join('/');
 const project_commit_index =  [project_docs_dir, 'index.md'].join('/');
 
+let project = require('./lib/page')('project');
+project.addHeader('repository', env.DRONE_REPO_LINK);
+project.addHeader('categories', [env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME, [env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/')]);
+project.addHeader('project', [[env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/')]);
+project.addHeader('last_build_status', env.DRONE_PREV_BUILD_STATUS);
+project.addHeader('build_status', env.DRONE_BUILD_STATUS);
+project.addHeader('title',  [env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/'));
 
+fs.writeFileSync(project_index, project.write())
 
-let stats;
-try {
-    fs.statSync(project_index);
-} catch (ex) {
-    console.log('CREATE NEW ', project_index);
-    fs.ensureFileSync(project_index)
-    fs.writeFileSync(project_index,'+++\nrepository = "here"\n+++\n\n');
+fs.ensureFileSync(build_doc_name)
+let build = require('./lib/page')('post');
+
+build.addHeader('repository', env.DRONE_REPO_LINK);
+build.addHeader('project', [[env.DRONE_REPO_OWNER, env.DRONE_REPO_NAME].join('/')]);
+build.addHeader('last_build_status', env.DRONE_PREV_BUILD_STATUS);
+build.addHeader('build_status', env.DRONE_BUILD_STATUS);
+build.addHeader('title', env.DRONE_BUILD_NUMBER);
+build.addHeader('author', env.DRONE_COMMIT_AUTHOR);
+build.addHeader('avatar', env.DRONE_COMMIT_AUTHOR_AVATAR);
+build.addHeader('link', env.DRONE_BUILD_LINK);
+
+try { 
+    composer.init().then(() => {
+        console.log('composer initialized');
+        return composer.checkAllForUpdate();
+    }).catch(() => {
+
+    }).then(() => {
+        build.addContent('# Module List')
+        build.addNewLine();
+        var packages = composer.getPackages().values();
+        let i = 0;
+
+        packages.forEach((v) => {
+            if (v.name.split('/').shift() !== 'drupal') {
+                return;
+            }
+            build.addNewLine();
+            build.addContent('## ' + v.name);
+            build.addNewLine();
+            build.addContent('Installed version:' + v.version);
+            build.addNewLine();
+            v.releases.forEach((release,k) => {
+                let buildOutput = '- New version:' + release.version;
+                if (release.terms) {
+                    let rTerms = [];
+                    release.terms.forEach((terms) => {
+                        terms.term.forEach((term) => {
+                            rTerms.push(term.value[0])
+                        })
+                    })
+                    buildOutput += ' (' + rTerms.join(', ') + ')';
+                }
+                if (release.release_link) {
+                    buildOutput += ' - [View](' + release.release_link[0] + ')';  
+                }
+                build.addContent(buildOutput);
+                build.addNewLine();
+            })
+            build.addNewLine();
+            i++;
+            console.log(i);
+        })
+    }).catch(() => {
+
+    }).then(() => {
+        fs.writeFileSync(build_doc_name, build.write())
+    })
+} catch (e) {
+    console.log(e);
 }
 
-let content = fs.readFileSync(project_index).toString();
-content = content.split('+++\n\n');
-content[1] = '- new content\n' + content[1];
-fs.writeFileSync(project_index, content.join('+++\n\n'))
-console.log('End publishing for hugo');
